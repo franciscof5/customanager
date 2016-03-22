7d7a8ef311bf908d9da490658637b3fe7f508841
Francisco Mat <francisco@f5sites.com>
2016-03-22 04:03:57 -0300

    footer host

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div> <!-- /container -->
 <br />
 <hr />
-<p class="text-center">Developed by <a href="http://www.franciscomat.com/customanager">Francisco Mat</a></p>
+<p class="text-center">Developed by <a href="http://www.franciscomat.com/customanager">Francisco Mat</a> | Hosted by <a href="http://www.f5sites.com">F5 Sites</a></p>
 <p class="text-center">Itapetininga, São Paulo, 2016</p>
 
 </body>
